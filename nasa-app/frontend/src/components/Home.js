8c98e0b36fe3bfe9b6df5ee72775b8feb2f9eb6d
Akshay Mishra <akshaymishra94@gmail.com>
2025-06-28 21:02:49 +0100

    background styling

--- nasa-app/frontend/src/components/Home.js
+++ nasa-app/frontend/src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-      <h1>NASA Space Explorer</h1>
-      <button onClick={() => navigate('/apod')} style={{ margin: '1rem', padding: '1rem 2rem' }}>
+    <div className="home-background">
+      <h1 style={{ color: '#fff', fontSize: '5rem', marginBottom: '30rem'}}>NASA Space Explorer</h1>
+      <button className="home-btn" onClick={() => navigate('/apod')} style={{ marginRight: '30rem', marginTop: '-19rem', padding: '1rem 2rem', marginBottom: '5rem'}}>
         Astronomy Picture of the Day
       </button>
-      <button onClick={() => navigate('/mars-photos')} style={{ margin: '1rem', padding: '1rem 2rem' }}>
+      <button onClick={() => navigate('/mars-photos')} style={{ margin: '1rem', padding: '1rem 2rem', marginBottom: '5rem' }}>
         Mars Rover Photos
       </button>
     </div>
